769736663f49acf968d51b57d77f412c31b8fc7d
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:21:54 +0100

    v1.1.0

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { LuaObj } from "@wowts/lua";
-export declare class AceLocale {
-    GetLocale(name: string, optional: boolean): LuaObj<string>;
-}
-declare const lib: AceLocale;
-export default lib;
